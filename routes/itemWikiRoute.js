ad84410632e8c27d066ae0b9cfb82d7338a548b3
norBMW <norBMW@outlook.hu>
2025-03-31 14:59:06 +0200

    yey

--- routes/itemWikiRoute.js
+++ routes/itemWikiRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./userRoute");
 const { getAllItem } = require("../controllers/itemWikiController");
-const app = express();
 
-app.use(cors())
-app.use(express.json())
+
 
 router.get("/allitem", getAllItem)
+
+module.exports = router
